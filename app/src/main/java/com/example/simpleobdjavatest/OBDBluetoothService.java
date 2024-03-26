0f6f457ba667e67223554262740fc06b32091672
Timmy5818 <qazwsxedc12341@hotmail.com>
2024-03-26 15:32:35 +0800

    test

--- app/src/main/java/com/example/simpleobdjavatest/OBDBluetoothService.java
+++ app/src/main/java/com/example/simpleobdjavatest/OBDBluetoothService.java
@@ -1,207 +1,242 @@
 package com.example.simpleobdjavatest;
 
 import android.annotation.SuppressLint;
-import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.IBinder;
 import android.util.Log;
 
-import androidx.annotation.Nullable;
-
-import org.assertj.core.api.Assertions;
-import org.obd.metrics.api.Workflow;
-import org.obd.metrics.api.model.AdaptiveTimeoutPolicy;
-import org.obd.metrics.api.model.Adjustments;
-import org.obd.metrics.api.model.BatchPolicy;
-import org.obd.metrics.api.model.CachePolicy;
-import org.obd.metrics.api.model.Init;
-import org.obd.metrics.api.model.Init.Header;
-import org.obd.metrics.api.model.Init.Protocol;
-import org.obd.metrics.api.model.Pids;
-import org.obd.metrics.api.model.ProducerPolicy;
-import org.obd.metrics.api.model.Query;
-import org.obd.metrics.command.group.DefaultCommandGroup;
-import org.obd.metrics.diagnostic.RateType;
 import org.obd.metrics.transport.AdapterConnection;
 
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-public class OBDBluetoothService extends Service {
+public class BluetoothConnection implements AdapterConnection {
+    private static final String LOGGER_TAG = "BluetoothConnection";
+    private static final UUID RFCOMM_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
-    public static final String ACTION_OBD_STATE = "com.example.OBD.ACTION_OBD_STATE";
-    public static final String EXTRA_OBD_STATE = "obd_state";
-    public static final String EXTRA_OBD_SPEED = "obd_speed";
-    private BluetoothConnection bluetoothConnection;
+    private final String deviceName;
+    private BluetoothSocket socket;
+    private InputStream input;
+    private OutputStream output;
 
-    private final BroadcastReceiver connectionReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (Objects.equals(intent.getAction(), ACTION_OBD_STATE)) {
-                int state = intent.getIntExtra(EXTRA_OBD_STATE, 0);
-                // Connected
-                if (state == 1) {
-                    Log.i("OBDBluetoothService","BroadcastReceiver OBD State is Connected");
-                    // Run test()
-                    try {
-                        test();
-                    } catch (IOException | InterruptedException | ExecutionException e) {
-                        throw new RuntimeException(e);
-                    }
-                } else {
-                    Log.i("OBDBluetoothService","BroadcastReceiver OBD State is not Connect");
-                }
-            }
+    private Context context;
+
+    public BluetoothConnection(String deviceName) {
+        this.deviceName = deviceName;
+        Log.i(LOGGER_TAG, "Created instance of BluetoothConnection with device: " + deviceName);
+    }
+
+    @Override
+    public void reconnect() {
+        try {
+            Log.i(LOGGER_TAG, "Reconnecting to the device: " + deviceName);
+            close();
+            TimeUnit.MILLISECONDS.sleep(1000);
+            connect();
+            Log.i(LOGGER_TAG, "Successfully reconnected to the device: " + deviceName);
+        } catch (InterruptedException | IOException e) {
+            Log.e(LOGGER_TAG, "Error reconnecting to the device: " + deviceName, e);
         }
-    };
+    }
 
-//    @SuppressLint("UnspecifiedRegisterReceiverFlag")
+//    @SuppressLint("MissingPermission")
 //    @Override
-//    public int onStartCommand(Intent intent, int flags, int startId) {
-//        bluetoothConnection = new BluetoothConnection("OBDII");
-//        registerReceiver(connectionReceiver, new IntentFilter(ACTION_OBD_STATE));
+//    public void connect() throws IOException {
+//        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//        BluetoothDevice device = null;
 //
-////        try {
-////            test();
-////        } catch (IOException | ExecutionException | InterruptedException e) {
-////            throw new RuntimeException(e);
-////        }
-//
-//        new Thread(() -> {
-//            try {
-//                Log.i("OBDBluetoothService","Try OBD-II Connection");
-//                bluetoothConnection = new BluetoothConnection("OBDII");
-//                bluetoothConnection.connectWithRetry(3); // Try 3 times
-//
-//                Intent intent2 = new Intent(OBDBluetoothService.ACTION_OBD_STATE);
-//                intent2.putExtra(OBDBluetoothService.EXTRA_OBD_STATE, 1);
-//                test();
-//            } catch (IOException | ExecutionException | InterruptedException e) {
-//                Intent intent3 = new Intent(OBDBluetoothService.ACTION_OBD_STATE);
-//                intent3.putExtra(OBDBluetoothService.EXTRA_OBD_STATE, 0);
+//        if (bluetoothAdapter != null) {
+//            for (BluetoothDevice bondedDevice : bluetoothAdapter.getBondedDevices()) {
+//                if (bondedDevice.getName() != null && bondedDevice.getName().contains("OBD")) {
+//                    device = bondedDevice;
+//                    Log.i(LOGGER_TAG, "OBD Device found: " + bondedDevice.getName());
+//                    break;
+//                }
 //            }
-//        }).start();
-//        return flags;
+//
+//            if (device == null) {
+//                throw new IOException("Device not found: " + deviceName);
+//            }
+//
+////            socket = device.createRfcommSocketToServiceRecord(RFCOMM_UUID);
+//            try {
+//                socket = (BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[] {int.class}).invoke(device, 1);
+//            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//            if (socket != null) {
+//                socket.connect();
+//
+//                if (socket.isConnected()) {
+//                    input = socket.getInputStream();
+//                    output = socket.getOutputStream();
+//                    Log.i(LOGGER_TAG, "Successfully connected to the device: " + deviceName);
+//
+//                    // Send Connected Broadcast
+//                    Intent intent = new Intent(OBDBluetoothService.ACTION_OBD_STATE);
+//                    intent.putExtra(OBDBluetoothService.EXTRA_OBD_STATE, 1);
+//                } else {
+//                    throw new IOException("Failed to connect to the device: " + deviceName);
+//                }
+//            }
+//
+//
+//
+//        } else {
+//            throw new IOException("BluetoothAdapter not found");
+//        }
 //    }
 
-    @SuppressLint("UnspecifiedRegisterReceiverFlag")
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        bluetoothConnection = new BluetoothConnection("OBD");
-
-        // initialization BroadcastReceiver
-        IntentFilter filter = new IntentFilter(OBDBluetoothService.ACTION_OBD_STATE);
-        registerReceiver(connectionReceiver, filter);
-
-        bluetoothConnection = new BluetoothConnection(this, "OBD");
-
-//        try {
-//            test();
-//        } catch (IOException | ExecutionException | InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-
-        new Thread(() -> {
-            try {
-                Log.i("OBDBluetoothService","Try OBD-II Connection");
-                bluetoothConnection = new BluetoothConnection("OBDII");
-                bluetoothConnection.connectWithRetry(10); // Try 10 times
-
-                Intent intent = new Intent(OBDBluetoothService.ACTION_OBD_STATE);
-                intent.putExtra(OBDBluetoothService.EXTRA_OBD_STATE, 1);
-                test();
-            } catch (IOException | ExecutionException | InterruptedException e) {
-                Intent intent = new Intent(OBDBluetoothService.ACTION_OBD_STATE);
-                intent.putExtra(OBDBluetoothService.EXTRA_OBD_STATE, 0);
-            }
-        }).start();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(connectionReceiver);
-    }
-
-    public void sendBroadcast(Intent intent) {
-        sendBroadcast(intent);
-    }
-
-    public void test() throws IOException, InterruptedException, ExecutionException {
-        AdapterConnection connection = bluetoothConnection;
-        // call connect()
-        connection.connect();
-        var collector = new DataCollector(this);
-
-        final Pids pids = Pids
-                .builder()
-                .resource(Objects.requireNonNull(Thread.currentThread().getContextClassLoader()).getResource("giulia_2.0_gme.json"))
-                .resource(Objects.requireNonNull(Thread.currentThread().getContextClassLoader()).getResource("extra.json"))
-                .resource(Objects.requireNonNull(Thread.currentThread().getContextClassLoader()).getResource("mode01.json"))
-                .build();
-
-        int commandFrequency = 6;
-        var workflow = Workflow.instance().pids(pids).observer(collector).initialize();
-
-        var query = Query.builder().pid(7005l).pid(7006l).pid(7007l).pid(7008l).build();
-
-        var optional = Adjustments
-                .builder()
-                .vehicleCapabilitiesReadingEnabled(Boolean.TRUE)
-                .vehicleMetadataReadingEnabled(Boolean.TRUE)
-                .adaptiveTimeoutPolicy(AdaptiveTimeoutPolicy.builder().enabled(Boolean.TRUE).checkInterval(5000).commandFrequency(commandFrequency).build())
-                .producerPolicy(ProducerPolicy.builder().priorityQueueEnabled(Boolean.TRUE).build())
-                .cachePolicy(CachePolicy.builder().resultCacheEnabled(Boolean.FALSE).build())
-                .batchPolicy(
-                        BatchPolicy.builder().responseLengthEnabled(Boolean.FALSE).enabled(Boolean.FALSE).build())
-                .build();
-
-        var init = Init.builder()
-                .delayAfterInit(1000)
-                .header(Header.builder().mode("22").header("DA10F1").build())
-                .header(Header.builder().mode("01").header("DB33F1").build())
-                .protocol(Protocol.CAN_29)
-                .sequence(DefaultCommandGroup.INIT).build();
-
-        workflow.start(connection, query, init, optional);
-
-        WorkflowFinalizer.finalizeAfter(workflow,25000);
-
-        var registry = workflow.getPidRegistry();
-
-        var intakePressure = registry.findBy(7005l);
-        double ratePerSec = workflow.getDiagnostics().rate().findBy(RateType.MEAN, intakePressure).get().getValue();
-
-        Assertions.assertThat(ratePerSec).isGreaterThanOrEqualTo(commandFrequency);
+    // 修改構造函數以包括Context
+    public BluetoothConnection(Context context, String deviceName) {
+        this.context = context;
+        this.deviceName = deviceName;
+        Log.i(LOGGER_TAG, "Created instance of BluetoothConnection with device: " + deviceName);
     }
 
     @SuppressLint("MissingPermission")
-    private BluetoothDevice getDeviceByName(String name) {
+    @Override
+    public void connect() throws IOException {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothDevice device = null;
+
         if (bluetoothAdapter != null) {
-            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-            if (pairedDevices != null && !pairedDevices.isEmpty()) {
-                for (BluetoothDevice device : pairedDevices) {
-                    if (device.getName().equals(name)) {
-                        return device;
-                    }
+            for (BluetoothDevice bondedDevice : bluetoothAdapter.getBondedDevices()) {
+                if (bondedDevice.getName() != null && bondedDevice.getName().contains("OBD")) {
+                    device = bondedDevice;
+                    Log.i(LOGGER_TAG, "OBD Device found: " + bondedDevice.getName());
+                    break;
+                }
+            }
+
+            if (device == null) {
+                throw new IOException("Device not found: " + deviceName);
+            }
+
+            // find BluetoothDevice object to connect
+            socket = device.createRfcommSocketToServiceRecord(RFCOMM_UUID);
+            socket.connect();
+
+            if (socket.isConnected()) {
+                input = socket.getInputStream();
+                output = socket.getOutputStream();
+                Log.i(LOGGER_TAG, "Successfully connected to the device: " + device.getName());
+
+                // 發送連線成功的廣播
+                Intent intent = new Intent(OBDBluetoothService.ACTION_OBD_STATE);
+                intent.putExtra(OBDBluetoothService.EXTRA_OBD_STATE, 1);
+                context.sendBroadcast(intent);
+            } else {
+                throw new IOException("Failed to connect to the device: " + device.getName());
+            }
+        } else {
+            throw new IOException("BluetoothAdapter not found");
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (input != null) {
+                input.close();
+            }
+            if (output != null) {
+                output.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+            Log.i(LOGGER_TAG, "Socket for the device: " + deviceName + " is closed.");
+        } catch (IOException e) {
+            Log.e(LOGGER_TAG, "Error closing the socket: " + deviceName, e);
+        }
+    }
+
+    @Override
+    public OutputStream openOutputStream() {
+        return output;
+    }
+
+    @Override
+    public InputStream openInputStream() {
+        return input;
+    }
+
+    @SuppressLint("MissingPermission")
+    public void connectWithRetry(int retryCount) throws IOException {
+        final int maxTries = retryCount;
+        int attempt = 0;
+        while (true) {
+            try {
+                Log.i("BluetoothConnection","Try OBD-II Connection");
+                attempt++;
+                connect(); // Try Connect
+                break; // Connected, break while
+            } catch (IOException e) {
+                if (attempt > maxTries) {
+                    Log.i("BluetoothConnection","OBD-II connect fail");
+                    throw e; // Fail
+                }
+                // Wait some time before retrying
+                try {
+                    Log.i("BluetoothConnection","retry connect");
+                    Thread.sleep(2000);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt(); // stay disconnected
                 }
             }
         }
-        return null;
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    @SuppressLint("MissingPermission")
+    private void connectToDevice() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Log.i(LOGGER_TAG, "Bluetooth not supported on this device");
+            return;
+        }
+
+        try {
+            Log.i(LOGGER_TAG, "Found bounded connections, size: " + bluetoothAdapter.getBondedDevices().size());
+            BluetoothDevice adapter = null;
+
+            // find Bluetooth deviceName
+            for (BluetoothDevice bondedDevice : bluetoothAdapter.getBondedDevices()) {
+                if (deviceName.equals(bondedDevice.getName())) {
+                    adapter = bondedDevice;
+                    break;
+                }
+            }
+
+            if (adapter != null) {
+                Log.i(LOGGER_TAG, "Opening connection to bounded device: " + adapter.getName());
+                socket = adapter.createRfcommSocketToServiceRecord(RFCOMM_UUID);
+                socket.connect();
+                Log.i(LOGGER_TAG, "Doing socket connect for: " + adapter.getName());
+
+                if (socket.isConnected()) {
+                    Log.i(LOGGER_TAG, "Successfully established connection for: " + adapter.getName());
+                    input = socket.getInputStream();
+                    output = socket.getOutputStream();
+                    Log.i(LOGGER_TAG, "Successfully opened the sockets to device: " + adapter.getName());
+                } else {
+                    Log.e(LOGGER_TAG, "Failed to connect to the device: " + adapter.getName());
+                }
+            } else {
+                Log.e(LOGGER_TAG, "Device not found: " + deviceName);
+            }
+        } catch (IOException e) {
+            Log.e(LOGGER_TAG, "Failed to connect to the device: " + deviceName, e);
+        } catch (SecurityException e) {
+            Log.e(LOGGER_TAG, "Security exception: permissions might be missing.", e);
+        }
     }
+
 }
